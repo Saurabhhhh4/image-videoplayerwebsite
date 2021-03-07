34266422f0b464d3bd856007f3069e55d8c6cdaf
saurabh <saurabhkumar2504@gmail.com>
2021-03-07 06:41:44 +0530

    Initial commit

---
+++ my-app/my-app/src/Components/Content.js
@@ -0,0 +1,254 @@
+import React from 'react';
+
+  function Content() {
+    return (
+      <div>
+      <section class="">
+           <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+             <div class="carousel-inner">
+               <div class="carousel-item active">
+                  <img class="d-block w-100" src="Images/ab2.jpg" alt="First slide" style={{height:"auto"}}/>
+               </div>
+               <div class="carousel-item">
+                  <img class="d-block w-100 " src="Images/ab2.jpg" alt="Second slide" style={{height:"auto"}}/>
+               </div>
+               <div class="carousel-item">
+                  <img class="d-block w-100" src="Images/ab2.jpg" alt="Third slide" style={{height:"auto"}}/>
+               </div>
+             </div>
+            </div>
+     </section>     
+
+        <section class="bg-light" id="about">
+          <div class="container">
+            <div class="row">
+              <div class="col-sm-12 col-md-12">
+                 <h3 class="text-center mt-4 text-secondary">Know about us</h3>
+               </div>
+              </div>
+             <div class="row">
+                <p class="mt-4 mb-5">Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                   Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                    when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+                    It has survived not only five centuries, but also the leap into electronic typesetting, 
+                    remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset 
+                    sheets containing Lorem Ipsum passages, and more recently with desktop publishing software 
+                    like Aldus PageMaker including versions of Lorem Ipsum</p>
+             </div>   
+          </div>   
+        </section>   
+
+        <section class="" id="destinations">   
+         <div class="container">
+             <div class="row">
+              <div class="col-sm-12 col-md-12">
+                 <h3 class="text-center mt-4 text-secondary">Image description</h3>
+               </div>
+              </div>
+              <div class="row">
+                 <div class="col-sm-4 mb-5">
+                    <div class="card mt-4">
+                       <img class="card-img-top" src="Images/bc.jpg" alt=""/>
+                       <div class="card-body">
+                          <h4 class="card-title text-secondary">London</h4>
+                          <p class="card-text text-secondary">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque sequi doloribus.</p>
+                       </div>
+                       <div class="card-footer">
+                          <a href="#" class="btn btn-primary">Find Out More!</a>
+                       </div>
+                    </div>
+                 </div>
+                 <div class="col-sm-4 mb-5">
+                    <div class="card mt-4">
+                       <img class="card-img-top" src="Images/pa.jpg" alt=""/>
+                       <div class="card-body">
+                          <h4 class="card-title text-secondary">Paris</h4>
+                          <p class="card-text text-secondary">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque sequi doloribus.</p>
+                       </div>
+                       <div class="card-footer">
+                          <a href="#" class="btn btn-primary">Find Out More!</a>
+                       </div>
+                    </div>
+                 </div>
+                 <div class="col-sm-4 mb-5">
+                    <div class="card mt-4">
+                       <img class="card-img-top" src="Images/abc.jpg" alt=""/>
+                       <div class="card-body">
+                          <h4 class="card-title text-secondary">India</h4>
+                          <p class="card-text text-secondary">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque sequi doloribus.</p>
+                       </div>
+                       <div class="card-footer">
+                          <a href="#" class="btn btn-primary">Find Out More!</a>
+                       </div>
+                    </div>
+                 </div>
+              </div>
+        </div>
+
+
+       </section>  
+       <section class=""  id="places">
+        <div class="container">
+          <div class="col-sm-12 col-md-12 mb-4">
+             <h3 class="text-center text-secondary mt-4">Video and Description</h3>
+         </div>
+       <div class="accordion" id="accordionExample">
+        <div class="card">
+         <div class="card-header" id="headingOne">
+            <h2 class="mb-0">
+            <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+              Videos
+            </button>
+            </h2>
+         </div>
+
+<section>
+
+<div>
+
+<body>
+<div>
+   <h5>1)Top speed Cars [BMW,BUGATI CHIRON]</h5>
+</div>
+<video width="800" controls >
+   <source src="Images/car.mp4" type="video/mp4"></source>
+</video>
+ <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
+            <div class="card-body">
+            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+            </div>
+         </div>
+
+
+         <div>
+   <h5>2)BMW Beemer 620D Top Speed 300km/hr</h5>
+</div>
+<video width="800" controls>
+   <source src="Images/car2.mp4" type="video/mp4"></source>
+</video>
+ <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
+            <div class="card-body">
+            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+            </div>
+         </div>
+
+
+
+         <div>
+   <h5>3)New generation cars advertisement</h5>
+
+</div>
+
+<video width="800" controls>
+   <source src="Images/car3.mp4" type="video/mp4"></source>
+</video>
+ <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
+            <div class="card-body">
+            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+            </div>
+         </div>
+
+</body>
+
+
+</div>
+
+</section>
+
+
+
+
+
+
+         <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
+            <div class="card-body">
+            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+            </div>
+         </div>
+      </div>
+      <div class="card">
+         <div class="card-header" id="headingTwo">
+            <h2 class="mb-0">
+            <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                Show More Videos
+            </button>
+            </h2>
+         </div>
+         <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
+            <div class="card-body">
+            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+            </div>
+         </div>
+      </div>
+      <div class="card">
+         <div class="card-header" id="headingThree">
+            <h2 class="mb-0">
+            <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+               Show More Images
+            </button>
+            </h2>
+         </div>
+         <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
+            <div class="card-body">
+            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+            </div>
+         </div>
+      </div>
+      </div>
+    </div> 
+    </section>
+
+    
+    <section class="bg-light mt-5" id="tourist">    
+     <div class="container">
+      <div class="row text-center">
+        <div class="col-sm-12 col-md-12 mb-4">
+            <h3 class="text-center mt-4 text-secondary">Visitors Opinions</h3>
+         </div>
+        <div class="col-md-4">
+          <div class="testimonial mb-5">
+           <div class="avatar mx-auto">
+           <img src="https://mdbootstrap.com/img/Photos/Avatars/img(31).jpg" class="img-fluid z-depth-1 rounded-circle"
+                              alt="Responsive image"></img>
+          </div>
+          <h4 class="font-weight-bold dark-grey-text mt-4">Elena Gilbert</h4>
+          <h6 class="font-weight-bold blue-text my-3">Programmer</h6>
+          <p class="font-weight-normal dark-grey-text">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod
+            eos id officiis hic tenetur quae quaerat ad velit ab hic tenetur.</p>
+        </div>
+      </div>
+
+      <div class="col-md-4">
+        <div class="testimonial mb-5">
+          <div class="avatar mx-auto">
+          <img src="https://mdbootstrap.com/img/Photos/Avatars/img%20(27).jpg"
+                              alt="avatar mx-auto white" class="rounded-circle img-fluid"></img>
+          </div>
+          <h4 class="font-weight-bold dark-grey-text mt-4">Jessie Pinkman</h4>
+          <h6 class="font-weight-bold blue-text my-3">Web Developer</h6>
+          <p class="font-weight-normal dark-grey-text">Ut enim ad minima veniam, quis nostrum exercitationem ullam
+            corporis suscipit laboriosam, nisi ut aliquid commodi.</p>
+        </div>
+
+      </div>
+      <div class="col-md-4">
+        <div class="testimonial mb-5">
+          <div class="avatar mx-auto">
+          <img src="https://mdbootstrap.com/img/Photos/Avatars/img(30).jpg" class="img-fluid z-depth-1" alt="Responsive image"></img>
+          </div>
+          <h4 class="font-weight-bold dark-grey-text mt-4">Jenneharnandez Skyler</h4>
+          <h6 class="font-weight-bold blue-text my-3">Software Engineer</h6>
+          <p class="font-weight-normal dark-grey-text">
+            At vero eos et accusamus et iusto odio dignissimos ducimus qui
+            blanditiis praesentium voluptatum deleniti atque corrupti.</p>
+        </div>
+        </div>
+      </div>
+    </div> 
+   </section> 
+   </div>
+    );
+  }
+  
+  export default Content;
